f6f5e003edbbcf218204bc04e556a36d5f89284c
Arya Bayu <aryabayu23@gmail.com>
2019-04-08 12:56:12 +0700

    fixing total

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -221,6 +221,12 @@ class Admin_model extends CI_Model {
 		  $hasil = json_decode($response,true);
 		  $ongkir = $hasil['rajaongkir']['results'][0]['costs'][1]['cost'][0]['value'];
 		  $this->db->update('ct_penjualan', array('ongkos_kirim' => $ongkir),array('id_penjualan'=>$id_penjualan));
+
+		  $get_total = $this->db->query("SELECT total FROM ct_penjualan WHERE id_penjualan=$id_penjualan")->row();
+		  $total = $get_total->total;
+
+		  $new_total  = $total + $ongkir;
+		  $this->db->update('ct_penjualan', array('total' => $new_total),array('id_penjualan'=>$id_penjualan));
 		}
 
 	}
